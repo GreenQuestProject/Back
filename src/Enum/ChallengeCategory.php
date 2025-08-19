9fe038388b4c00086eb7daa1a3c2fad18970c5f8
Karina Teyssere <karinateyssere@gmail.com>
2025-08-19 22:41:13 +0200

    Delete health category

--- src/Enum/ChallengeCategory.php
+++ src/Enum/ChallengeCategory.php
@@ -5,7 +5,6 @@ namespace App\Enum;
 enum ChallengeCategory: string
 {
     case ECOLOGY = 'ecology';
-    case HEALTH = 'health';
     case COMMUNITY = 'community';
     case EDUCATION = 'education';
     case PERSONAL_DEVELOPMENT = 'personal_development';
