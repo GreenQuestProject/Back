0f758da4a56badd1d84010b9778695b829894ebe
Karina Teyssere <karinateyssere@gmail.com>
2025-08-31 11:43:48 +0200

    Update migrations

--- migrations/Version20250830135359.php
+++ migrations/Version20250830135359.php
@@ -22,7 +22,10 @@ final class Version20250830135359 extends AbstractMigration
         // this up() migration is auto-generated, please modify it to your needs
         $this->addSql('ALTER TABLE challenge CHANGE co2_estimate_kg co2_estimate_kg NUMERIC(10, 3) DEFAULT \'0\' NOT NULL, CHANGE water_estimate_l water_estimate_l NUMERIC(10, 3) DEFAULT \'0\' NOT NULL, CHANGE waste_estimate_kg waste_estimate_kg NUMERIC(10, 3) DEFAULT \'0\' NOT NULL');
         $this->addSql('ALTER TABLE user ADD created_at DATETIME NOT NULL COMMENT \'(DC2Type:datetime_immutable)\'');
+        $this->addSql("UPDATE user SET created_at = NOW() WHERE created_at IS NULL OR created_at = '0000-00-00 00:00:00'");
+        $this->addSql("ALTER TABLE user MODIFY created_at DATETIME NOT NULL COMMENT '(DC2Type:datetime_immutable)'");
     }
+
 
     public function down(Schema $schema): void
     {
